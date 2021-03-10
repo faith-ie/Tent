5adbc96d1432352926df3e8dffc10a945ab69225
Fuwn <fuwnzy@gmail.com>
2021-03-09 17:11:44 -0800

    tent

---
+++ tent.f95
@@ -0,0 +1,9 @@
+program tent
+    Print *, "    _____________________"
+    Print *, "   /                   / \\        z"
+    Print *, "  /                   /   \\      z"
+    Print *, " /                   /     \\    z"
+    Print *, "/                   /       \\  z"
+    Print *, "___________________/---------\\"
+    Print *, "                 "
+end program Tent
