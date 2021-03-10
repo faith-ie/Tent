1a27ff688a66eeddf570aa46fb08c089d001a7e6
Fuwn <fuwnzy@gmail.com>
2021-03-09 17:33:05 -0800

    tent

---
+++ tent.v
@@ -0,0 +1,7 @@
+module tent;
+  initial 
+    begin
+      $display("    _____________________\n   /                   / \\        z\n  /                   /   \\      z\n /                   /     \\    z\n/                   /       \\  z\n___________________/---------\\\n");
+      $finish ;
+    end
+endmodule
