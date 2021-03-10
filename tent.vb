e154bb34ba325f123c7b4b380016061b39028524
Fuwn <fuwnzy@gmail.com>
2021-03-09 17:32:33 -0800

    tent

---
+++ tent.vb
@@ -0,0 +1,11 @@
+Module VBModule
+    Sub Main()
+        Console.WriteLine("    _____________________")
+        Console.WriteLine("   /                   / \\        z")
+        Console.WriteLine("  /                   /   \\      z")
+        Console.WriteLine(" /                   /     \\    z")
+        Console.WriteLine("/                   /       \\  z")
+        Console.WriteLine("___________________/---------\\")
+        Console.WriteLine("                 ")
+    End Sub
+End Module
